9078587dfab51c93412b88324d0a723be3b51c02
Boris Kaiser <mail@boriskaiser.com>
2016-03-13 14:44:36 +0100

    add webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const path = require('path');
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+    devtool: NODE_ENV === 'development' ? 'cheap-module-inline-source-map' : null,
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
+    ],
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js']
+    },
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        modulesTemplates: ['*-loader', '*'],
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
